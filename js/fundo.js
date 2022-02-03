41cb3677768077fbf4bdfc879e0f6e0a5c8aa425
Marcos Felipe Dantas da Costa <felipeicedantas@gmail.com>
2022-02-03 10:55:27 -0300

    melhorias visuais

--- js/fundo.js
+++ js/fundo.js
@@ -7,19 +7,35 @@ temas[2].addEventListener('click', mudou3);
 temas[3].addEventListener('click', mudou4);
 
 function mudou1() {
+    temas[0].style.backgroundColor = 'rgb(0, 221, 228)';
+    temas[1].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
+    temas[2].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
+    temas[3].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
     fundo.style.backgroundImage = 'url("imagens/fundo-1.gif")'
 }
 
 function mudou2() {
+    temas[0].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
+    temas[1].style.backgroundColor = 'rgb(0, 221, 228)';
+    temas[2].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
+    temas[3].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
     fundo.style.backgroundImage = 'url("imagens/fundo-2.gif")'
 }
 
 function mudou3() {
+    temas[0].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
+    temas[1].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
+    temas[2].style.backgroundColor = 'rgb(0, 221, 228)';
+    temas[3].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
     fundo.style.backgroundImage = 'url("imagens/fundo-3.gif")'
 }
 
 function mudou4() {
+    temas[0].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
+    temas[1].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
+    temas[2].style.backgroundColor = 'rgba(88, 0, 100, 0.502)';
+    temas[3].style.backgroundColor = 'rgb(0, 221, 228)';
     fundo.style.backgroundImage = 'url("imagens/fundo-4.gif")'
 }
 
-export {mudou1, mudou2, mudou3, mudou4};
+export { mudou1, mudou2, mudou3, mudou4 };
